9cb12081f0dd63a5703c56af2408d0a0328210f8
Sungpil An <ansungpil1@gmail.com>
2021-02-05 13:09:15 -0500

    Added fitness.js in models

---
+++ models/fitness.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const FitnessSchema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: Number,
+                weight: {
+                    type: Number,
+                    default: 0,
+                },
+                reps: {
+                    type: Number,
+                    default: 0
+                },
+                sets: {
+                    type: Number,
+                    default: 0
+                },
+                distance: {
+                    type: Number,
+                    default: 0
+                }
+            }
+        ],
+    totalDuration: {
+        type: Number,
+        default: 0,
+    }
+});
+
+const Fitness = mongoose.model("Fitness", FitnessSchema);
+
+module.exports = Fitness;
